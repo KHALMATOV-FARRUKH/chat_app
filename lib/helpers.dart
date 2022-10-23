423279c2a358387fbb7da85db8da079f6725e752
Fara <115084566+KHALMATOV-FARRUKH@users.noreply.github.com>
2022-10-24 02:37:21 +0500

    Added dependencies: flutter pub add jiffy
    Dart files created
    message_data

--- lib/helpers.dart
+++ lib/helpers.dart
@@ -11,6 +11,6 @@ abstract class Helpers {
   static DateTime randomDate() {
     final random = Random();
     final currentDate = DateTime.now();
-    return currentDate.subtract(Duration(seconds: random.nextInt(20000)));
+    return currentDate.subtract(Duration(seconds: random.nextInt(200000)));
   }
 }
